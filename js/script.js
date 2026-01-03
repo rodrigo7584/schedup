efd93157fa789e4dd68d48d3cec40d16270101eb
rodrigo7584 <waboo@waboo.com.br>
2026-01-03 11:06:15 -0300

    section-4

--- js/script.js
+++ js/script.js
@@ -7,14 +7,24 @@
     menu.classList.toggle('active');   // alterna a classe no menu
   });
 
-var swiper = new Swiper(".swiper-reviews", {
+var swiper = new Swiper(".swiper-reviews .swiper", {
+    slidesPerView: 1, 
+    spaceBetween: 20,
     pagination: {
       el: ".swiper-reviews .swiper-pagination",
       clickable: true,
     },
     autoplay:{
-      delay: 300,
+      delay: 3000,
       disableOnInteraction: true,
+    },
+    breakpoints:{
+      578: {
+        slidesPerView: 2, 
+      },
+      990: {
+        slidesPerView: 3, 
+      }
     },
     loop:true
   });
